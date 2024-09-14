d1c0a6469b50b3479c6c74b9d3037b33cb0166ce
obedeDintala123 <121716336+obedeDintala123@users.noreply.github.com>
2024-09-14 18:31:17 +0100

    Update api.js

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create a new instance of axios
 const api = axios.create({
-  baseURL: "https://back-end-red-ten.vercel.app/",
+  baseURL: "https://back-end-llxf.onrender.com",
 });
 
 export default api;
